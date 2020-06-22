8f442ab4ba588cd5729f0e7d1e54b022e421ff7b
Elise Tobiasson <Elise@Elises-MacBook-Pro.local>
2020-06-22 11:27:02 +0200

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn daydreams-sms.wsgi --log-file -
+web: gunicorn gettingstarted.wsgi --log-file -
--- run.py
+++ run.py
@@ -1,33 +1,17 @@
-from flask import Flask, request, redirect
-import twilio.twiml
-import pika, os, urllib.parse
+from twilio.rest import Client
 
-app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = '?'
+auth_token = '?'
+client = Client(account_sid, auth_token)
 
-    from_number = request.values.get('From', None)
-    body = request.values.get('Body', None)
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+4759449207',
+                     to='+4794502224'
+                 )
 
-    url_str = os.environ.get('CLOUDAMQP_URL','amqp://pbvvwkhx:CIVpeKpO-ESZH4iBTSv0ddA7ndIoaCWv@crow.rmq.cloudamqp.com/pbvvwkhx')
-    url = urlparse.urlparse(url_str)
-
-    params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
-        credentials=pika.PlainCredentials(url.username, url.password))
-
-    connection = pika.BlockingConnection(params)
-    channel = connection.channel()
-    channel.queue_declare(queue='texts')
-
-    message = from_number + ": " + body
-
-    channel.basic_publish(exchange='', routing_key='texts', body=message)
-    connection.close()
-
-    resp = twilio.twiml.Response()
-    resp.message("We're so happy! We got your message and it's currently printing. Tx, Elise T")
-    return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print(message.sid)
